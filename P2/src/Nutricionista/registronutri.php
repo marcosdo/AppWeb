1f84a4023de254b7a3cc43624464a6f212eb875a
sramos075 <99352465+sramos075@users.noreply.github.com>
2022-03-08 17:59:24 +0100

    estilo temporal

--- P2/src/Nutricionista/registronutri.php
+++ P2/src/Nutricionista/registronutri.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <link rel="stylesheet" type="text/css" href="../../estilo.css" />
+        <link rel="stylesheet" type="text/css" href="./../resources/CSS/estiloaux.css" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Planificacion</title>
     </head>
