10268a62ec7e017c2fd054f1d593c0d65b0a5118
sramos075 <99352465+sramos075@users.noreply.github.com>
2022-03-10 17:16:27 +0100

    BD

--- P2/practica_2_aw.sql
+++ P2/practica_2_aw.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-03-2022 a las 16:32:19
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+-- Tiempo de generación: 10-03-2022 a las 17:14:58
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,45 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `lifety`
+-- Base de datos: `practica 2 aw`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ejercicios`
+--
+
+CREATE TABLE `ejercicios` (
+  `Musculo` varchar(20) DEFAULT NULL,
+  `Nombre` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `ejercicios`
+--
+
+INSERT INTO `ejercicios` (`Musculo`, `Nombre`) VALUES
+('Pierna', 'Sentadilla'),
+('Pierna', 'Prensa'),
+('Pecho', 'Press banca'),
+('Pecho', 'Aperturas con mancue'),
+('Pecho', 'Press banca inclinad'),
+('Pecho', 'Maquina de empuje'),
+('Pierna', 'Extension de cruadri'),
+('Pierna', 'Hip thrust'),
+('Hombro', 'Elevacion lateral'),
+('Hombro', 'Press hombro'),
+('Hombro', 'Remo al menton'),
+('Triceps', 'Fondos'),
+('Triceps', 'Press frances'),
+('Triceps', 'Extensiones'),
+('Biceps', 'Curl araña'),
+('Biceps', 'Predicador'),
+('Biceps', 'Martillo'),
+('Espalda', 'Jalon'),
+('Espalda', 'Remo T'),
+('Espalda', 'Remo con barra');
 
 -- --------------------------------------------------------
 
@@ -31,7 +68,7 @@ CREATE TABLE `premium` (
   `Peso` float NOT NULL,
   `Altura` float NOT NULL,
   `Alergias` text NOT NULL,
-  `Observaciones adicionales` text NOT NULL,
+  `Observaciones_adicionales` text NOT NULL,
   `Num_logros` int(20) NOT NULL,
   `Logros` int(2) DEFAULT NULL,
   `Id_usuario` int(5) NOT NULL,
@@ -70,8 +107,18 @@ CREATE TABLE `usuario` (
   `Correo` varchar(50) NOT NULL,
   `Contraseña` varchar(25) NOT NULL,
   `Id_usuario` int(5) NOT NULL,
-  `Premium` tinyint(1) NOT NULL
+  `Premium` tinyint(1) NOT NULL,
+  `Nivel` char(1) DEFAULT NULL,
+  `Dias` int(1) DEFAULT NULL,
+  `Eobjetivo` int(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`Nombre`, `Apellido 1`, `Apellido 2`, `DNI`, `Correo`, `Contraseña`, `Id_usuario`, `Premium`, `Nivel`, `Dias`, `Eobjetivo`) VALUES
+('Alex', '', '', '', '', '', 0, 0, 'P', NULL, 1);
 
 --
 -- Índices para tablas volcadas
