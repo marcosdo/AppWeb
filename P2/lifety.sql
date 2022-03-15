871d0db854e8dff3d30d3e882e3f472a6173524b
amagarzog <amagarzo@ucm.es>
2022-03-15 10:22:27 +0100

    BD

--- P2/lifety.sql
+++ P2/lifety.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-03-2022 a las 10:59:57
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+-- Tiempo de generación: 15-03-2022 a las 10:21:18
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,6 +33,47 @@ CREATE TABLE `dietas` (
   `Tipo` enum('Desayuno','Comida','Cena') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `dietas`
+--
+
+INSERT INTO `dietas` (`Objetivo`, `descripcion`, `Tipo`) VALUES
+(1, 'Copos de avena con leche', 'Desayuno'),
+(1, 'Yogur con copos de avena', 'Desayuno'),
+(1, 'Tortitas de avena', 'Desayuno'),
+(2, 'Porridge de avena y frutos secos', 'Desayuno'),
+(2, 'Requesón con fruta', 'Desayuno'),
+(2, 'Sándwich de queso gouda con huevo duro', 'Desayuno'),
+(2, 'Batido de platano y avena', 'Desayuno'),
+(3, 'Smoothie Bowls', 'Desayuno'),
+(3, 'Hotcakes de avena y quinoa', 'Desayuno'),
+(3, 'Pudín', 'Desayuno'),
+(3, 'Wrap de pavo', 'Desayuno'),
+(1, 'Ensalada de calabacín a la plancha con q', 'Comida'),
+(1, 'Albondigas de merluza y brocoli', 'Comida'),
+(1, 'Pechuga de pollo con soja y verduras salteadas', 'Comida'),
+(1, 'Caldo de verduras con tortilla francesa', 'Comida'),
+(2, 'Ensalada de patata hervida, tomate, pepino y huevo duro', 'Comida'),
+(2, 'Wok de verduras al curry con tiras de pollo', 'Comida'),
+(2, '.Berenjena y calabacín asado con pimentón dulce', 'Comida'),
+(2, 'Puré de calabaza con patata hervida', 'Comida'),
+(2, 'Tomates rellenos de lentejas', 'Comida'),
+(2, 'Revuelto de gambas y champiñones', 'Comida'),
+(3, 'Pollo con verduras y queso batido al papillote', 'Cena'),
+(3, 'Salmón al horno con salsa de yogur', 'Cena'),
+(3, 'Sopa de pasta y hamburguesa con naranja picada', 'Cena'),
+(3, 'Crema de verduras y pechuga de pollo con manzana', 'Cena'),
+(3, 'Croquetas de pescado con pure de patata', 'Cena'),
+(3, 'Pollo a la naranja', 'Cena'),
+(3, 'Garbanzos con espinacas', 'Cena'),
+(3, 'Arroz tres delicias', 'Cena'),
+(1, 'Ensalada de patata hervida, tomate, pepino y huevo duro', 'Cena'),
+(1, 'Wok de verduras al curry con tiras de pollo', 'Cena'),
+(2, 'Hamburguesa de jamón york con mayonesa y queso', 'Cena'),
+(2, 'Minipizza cuatro quesos con naranja troceada', 'Cena'),
+(3, 'Pollo con almendras chino', 'Comida'),
+(3, 'Atún a la plancha con ajo y perejil', 'Comida');
+
 -- --------------------------------------------------------
 
 --
@@ -40,15 +81,15 @@ CREATE TABLE `dietas` (
 --
 
 CREATE TABLE `ejercicios` (
-  `musculo` varchar(40) DEFAULT NULL,
-  `nombre` varchar(40) DEFAULT NULL
+  `Musculo` varchar(40) DEFAULT NULL,
+  `Nombre` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `ejercicios`
 --
 
-INSERT INTO `ejercicios` (`musculo`, `nombre`) VALUES
+INSERT INTO `ejercicios` (`Musculo`, `Nombre`) VALUES
 ('Hombro', 'Elevación lateral'),
 ('Hombro', 'Press hombro'),
 ('Hombro', 'Remo al mentón'),
@@ -77,19 +118,69 @@ INSERT INTO `ejercicios` (`musculo`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `planificacion`
+--
+
+CREATE TABLE `planificacion` (
+  `Id_usuario` int(5) NOT NULL,
+  `desayunos` text DEFAULT NULL,
+  `comidas` text DEFAULT NULL,
+  `cenas` text DEFAULT NULL,
+  `rutina` text DEFAULT NULL,
+  `Dobjetivo` int(1) DEFAULT NULL,
+  `Eobjetivo` int(1) DEFAULT NULL,
+  `Dias` int(1) DEFAULT NULL,
+  `Nivel` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `planificacion`
+--
+
+INSERT INTO `planificacion` (`Id_usuario`, `desayunos`, `comidas`, `cenas`, `rutina`, `Dobjetivo`, `Eobjetivo`, `Dias`, `Nivel`) VALUES
+(0, 'Tortitas de avena | Copos de avena con leche | Yogur con copos de avena | Copos de avena con leche | Yogur con copos de avena | Tortitas de avena | Tortitas de avena', 'Ensalada de calabacín a la plancha con q | Albondigas de merluza y brocoli | Ensalada de calabacín a la plancha con q | Pechuga de pollo con soja y verduras salteadas | Pechuga de pollo con soja y verduras salteadas | Albondigas de merluza y brocoli | Caldo de verduras con tortilla francesa', 'Wok de verduras al curry con tiras de pollo | Wok de verduras al curry con tiras de pollo | Ensalada de patata hervida, tomate, pepino y huevo duro | Wok de verduras al curry con tiras de pollo | Wok de verduras al curry con tiras de pollo | Wok de verduras al curry con tiras de pollo | Ensalada de patata hervida, tomate, pepino y huevo duro', NULL, NULL, NULL, NULL, NULL),
+(23, 'Comida A, Comida B', 'Comida C', 'Comida D', 'Press banca | Aperturas con mancuernas | Elevación lateral | Press hombro - Jalon | Remo en T | Curl araña | Predicador - Sentadilla | Prensa | Fondos | Press francés', NULL, NULL, NULL, NULL),
+(25, 'Porridge de avena y frutos secos | Batido de platano y avena | Requesón con fruta | Porridge de avena y frutos secos | Requesón con fruta | Requesón con fruta | Sándwich de queso gouda con huevo duro', 'Puré de calabaza con patata hervida | Revuelto de gambas y champiñones | Tomates rellenos de lentejas | Revuelto de gambas y champiñones | Tomates rellenos de lentejas | Wok de verduras al curry con tiras de pollo | Revuelto de gambas y champiñones', 'Minipizza cuatro quesos con naranja troceada | Minipizza cuatro quesos con naranja troceada | Hamburguesa de jamón york con mayonesa y queso | Minipizza cuatro quesos con naranja troceada | Hamburguesa de jamón york con mayonesa y queso | Hamburguesa de jamón york con mayonesa y queso | Minipizza cuatro quesos con naranja troceada', 'Press banca | Aperturas con mancuernas | Elevación lateral | Press hombro - Jalon | Remo en T | Curl araña | Predicador - Sentadilla | Prensa | Fondos | Press francés', NULL, NULL, NULL, NULL),
+(77, 'Batido de platano y avena | Batido de platano y avena | Sándwich de queso gouda con huevo duro | Batido de platano y avena | Porridge de avena y frutos secos | Requesón con fruta | Batido de platano y avena\r\n', 'Revuelto de gambas y champiñones | Ensalada de patata hervida, tomate, pepino y huevo duro | Revuelto de gambas y champiñones | Puré de calabaza con patata hervida | Puré de calabaza con patata hervida | Wok de verduras al curry con tiras de pollo | .Berenjena y calabacín asado con pimentón dulce\r\n', 'Hamburguesa de jamón york con mayonesa y queso | Minipizza cuatro quesos con naranja troceada | Hamburguesa de jamón york con mayonesa y queso | Hamburguesa de jamón york con mayonesa y queso | Hamburguesa de jamón york con mayonesa y queso | Hamburguesa de jamón york con mayonesa y queso | Hamburguesa de jamón york con mayonesa y queso\r\n', 'Press banca | Aperturas con mancuernas | Elevación lateral | Press hombro - Jalon | Remo en T | Curl araña | Predicador - Sentadilla | Prensa | Fondos | Press francés', NULL, 2, 3, 'P'),
+(44, 'Pudín | Pudín | Pudín | Hotcakes de avena y quinoa | Wrap de pavo | Wrap de pavo | Smoothie Bowls', 'Atún a la plancha con ajo y perejil | Pollo con almendras chino | Atún a la plancha con ajo y perejil | Atún a la plancha con ajo y perejil | Pollo con almendras chino | Pollo con almendras chino | Pollo con almendras chino', 'Crema de verduras y pechuga de pollo con manzana | Pollo a la naranja | Pollo con verduras y queso batido al papillote | Garbanzos con espinacas | Pollo con verduras y queso batido al papillote | Pollo a la naranja | Sopa de pasta y hamburguesa con naranja picada', 'Press banca | Aperturas con mancuernas | Press banca inclinado | Elevación lateral | Press hombro | Remo al mentón - Jalon | Remo en T | Remo con barra | Curl araña | Predicador | Martillo - Sentadilla | Prensa | Extensión de cuadriceps | Fondos | Press francés | Extensiones - Press banca | Aperturas con mancuernas | Press banca inclinado | Elevación lateral | Press hombro | Remo al mentón | Jalon | Remo en T | Remo con barra - Curl araña | Predicador | Martillo | Sentadilla | Prensa | Extensión de cuadriceps | Fondos | Press francés | Extensiones', 3, 2, 5, 'M');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `premium`
 --
 
 CREATE TABLE `premium` (
-  `peso` float NOT NULL,
-  `altura` float NOT NULL,
-  `alergias` text NOT NULL,
-  `observaciones` text NOT NULL,
-  `num_logros` int(20) NOT NULL,
-  `logros` int(2) NOT NULL,
-  `id_usuario` int(5) NOT NULL,
-  `id_profesional` int(5) NOT NULL
+  `Peso` float NOT NULL,
+  `Altura` float NOT NULL,
+  `Alergias` text NOT NULL,
+  `Observaciones_adicionales` text NOT NULL,
+  `Num_logros` int(20) NOT NULL,
+  `Logros` int(2) NOT NULL,
+  `Id_usuario` int(5) NOT NULL,
+  `Id_profesional` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `premium`
+--
+
+INSERT INTO `premium` (`Peso`, `Altura`, `Alergias`, `Observaciones_adicionales`, `Num_logros`, `Logros`, `Id_usuario`, `Id_profesional`) VALUES
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0),
+(75, 170, '', '', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -98,15 +189,23 @@ CREATE TABLE `premium` (
 --
 
 CREATE TABLE `profesional` (
-  `nombre` text NOT NULL,
-  `apellidos` text NOT NULL,
-  `correo` varchar(50) NOT NULL,
-  `dni` varchar(9) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `id_profesional` int(5) NOT NULL,
-  `usuarios` text NOT NULL,
-  `num_usuarios` int(3) NOT NULL
+  `Nombre` text NOT NULL,
+  `Apellidos` text NOT NULL,
+  `Contraseña` varchar(25) NOT NULL,
+  `Correo` varchar(50) NOT NULL,
+  `DNI` varchar(9) NOT NULL,
+  `Id_profesional` int(5) NOT NULL,
+  `Usuarios` text NOT NULL,
+  `Num_usuarios` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `profesional`
+--
+
+INSERT INTO `profesional` (`Nombre`, `Apellidos`, `Contraseña`, `Correo`, `DNI`, `Id_profesional`, `Usuarios`, `Num_usuarios`) VALUES
+('Sandra', 'Ramos Ramos', '1234', 'sandra@lifety', '45678923P', 0, 'Alex ', 1),
+('Ivan', 'Ledesma Casado', '123', 'ivan@lifety', '45678923E', 1, '', 0);
 
 -- --------------------------------------------------------
 
@@ -115,76 +214,71 @@ CREATE TABLE `profesional` (
 --
 
 CREATE TABLE `usuario` (
-  `id_usuario` int(5) NOT NULL,
-  `nombre` text NOT NULL,
-  `apellidos` text NOT NULL,
-  `dni` varchar(9) NOT NULL,
-  `correo` varchar(50) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `premium` tinyint(1) NOT NULL,
-  `nivel` char(1) DEFAULT NULL,
-  `dias` int(1) DEFAULT NULL,
-  `eobjetivo` int(1) DEFAULT NULL,
-  `dobjetivo` int(1) NOT NULL
+  `Nombre` text NOT NULL,
+  `Apellidos` text NOT NULL,
+  `DNI` varchar(9) NOT NULL,
+  `Correo` varchar(50) NOT NULL,
+  `Password` varchar(25) NOT NULL,
+  `Id_usuario` int(5) NOT NULL,
+  `Premium` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `nombre`, `apellidos`, `dni`, `correo`, `password`, `premium`, `nivel`, `dias`, `eobjetivo`, `dobjetivo`) VALUES
-(1, 'Alex', '', '', '', '1', 1, 'P', NULL, 1, 0),
-(2, 'Sandra', 'Ramos', '444444O', '', '2', 0, NULL, NULL, NULL, 0);
+INSERT INTO `usuario` (`Nombre`, `Apellidos`, `DNI`, `Correo`, `Password`, `Id_usuario`, `Premium`) VALUES
+('Alex', '', '', '', '1', 0, 1),
+('Sandra', 'Ramos', '444444O', '', '2', 1, 0),
+('Prueba', 'A', '', '', '', 23, 0),
+('Vacio', 'B', '', '', '', 25, 0),
+('Diego', '', '', '', '2', 44, 0),
+('Kylian', 'Mbappe', '', '', '', 77, 0);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `planificacion`
+--
+ALTER TABLE `planificacion`
+  ADD KEY `Id_FK` (`Id_usuario`);
+
+--
 -- Indices de la tabla `premium`
 --
 ALTER TABLE `premium`
-  ADD KEY `Usuario_FK` (`id_usuario`),
-  ADD KEY `Profesional_FK` (`id_profesional`);
+  ADD KEY `Id_FK` (`Id_usuario`),
+  ADD KEY `Profesional_FK` (`Id_profesional`);
 
 --
 -- Indices de la tabla `profesional`
 --
 ALTER TABLE `profesional`
-  ADD PRIMARY KEY (`id_profesional`);
+  ADD PRIMARY KEY (`Id_profesional`);
 
 --
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `profesional`
---
-ALTER TABLE `profesional`
-  MODIFY `id_profesional` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  ADD PRIMARY KEY (`Id_usuario`);
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `planificacion`
+--
+ALTER TABLE `planificacion`
+  ADD CONSTRAINT `Id_FK` FOREIGN KEY (`Id_usuario`) REFERENCES `usuario` (`Id_usuario`);
+
+--
 -- Filtros para la tabla `premium`
 --
 ALTER TABLE `premium`
-  ADD CONSTRAINT `Profesional_FK` FOREIGN KEY (`id_profesional`) REFERENCES `profesional` (`id_profesional`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Usuario_FK` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Profesional_FK` FOREIGN KEY (`Id_profesional`) REFERENCES `profesional` (`Id_profesional`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
