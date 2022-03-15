ede80f22d144e7d841b53839797303a29c076c2d
DiegoAA00 <diegoa02@ucm.es>
2022-03-15 17:37:22 +0100

    Integración EntrenadorPersonal y EntrenadorUsuario

--- P2/EntrenadorPersonalEnt.php
+++ P2/EntrenadorPersonalEnt.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="estilo.css" />
+<link rel="stylesheet" type="text/css" href="css/estiloD.css" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>login</title>
 </head>
@@ -9,17 +9,35 @@
 <body>
 <div id="contenedor">
     <?php 
-		include("../layout/cabecera.php");
-		include("../layout/sidebarIzq.php");
+		include("cabecera.php");
+		include("sidebarIzq.php");
 	?>
     <main>
-		<header><h1>Tu nutricionista/entrenador personal ENTRENADOR</h1></header>
-			
-			<div id = "chat">
-				aqui va un chat
-			</div>
-			<div id = "logros">
-			<?php 
+		<header><h1>Tu  nutricionista/entrenador personal ENTRENADOR </h1></header>
+			<p></p>
+			<div id = "logros" >
+			 
+				
+			<h1><span class = 'text'>L O G R O S</span></h1>
+				<div id = 'selectA'>
+				<h3><span class = 'textD'>Seleccione el Logro :</span></h3>
+				<h3><span class = 'textI' >Seleccione al Usuario :</span></h3>
+				</div>
+				<form method='post'>
+				<div id = 'select' >
+				<select name = 'logrosE' id = 'logrosE' type = 'text' class = 'selectA'>
+				<option value='5logros'>Completar 5 Logros</option>
+				<option value='AccesoTodos'>Acceso a todas las areas</option>
+				<option value='ComenzarChat'>Comenzar chat</option>
+				<option value='Completa1Plan'>Completar 1 plan</option>
+				<option value='Completa5Plan'>Completar 5 plan</option>
+				<option value='ContrataNutri'>Contratar un Nutricionista</option>
+				<option value='Foro'>Entrar en el foro</option>
+				<option value='Permanencia'>Sesión 5 dias seguidos</option>
+				<option value='Permanencia1m'>Sesión 1 mes seguidos</option>
+				</select>
+				<select name = 'idE' id = 'idE' type = 'text' class = 'selectB'>
+				<?php 
 				function conectar ($host, $usuario, $contraseña,$nombreBD){
 					$mysqli = new mysqli($host,$usuario,$contraseña,$nombreBD);
 					if ($mysqli->connect_errno) {
@@ -27,35 +45,119 @@
 					}
 					return $mysqli;
 				}
+				$BD = conectar("localhost","root","","lifety");
+				session_start();
+				$_SESSION['login'] = 'Entrenador1';
 
-				$BD = conectar("localhost","root","","aw");
-				$consulta = mysqli_query($BD,"SELECT * FROM Logros"); 
-
-				echo "<form method='post'>
-				<select name = 'idE' id = 'idE' type = 'text'>";
-				while($BDLogros =  mysqli_fetch_array($consulta)){
-					echo "<option value='$BDLogros[id]'>$BDLogros[id]</option>";
+				$consulta = mysqli_query($BD,"SELECT * FROM profesional WHERE id_profesional = '$_SESSION[login]'");
+				$BDLogros =  mysqli_fetch_array($consulta);
+				$cadena = explode(",",$BDLogros["usuarios"]);
+				foreach($cadena as $usuario){
+					echo "<option value='$usuario'>$usuario</option>";
 				}
-				echo "</select>";
-		
+				?>
+				</select>
+				</div>
+				<div id = 'select'>
+				<?php 
+				echo "<h3>";
 				if(isset($_POST['buttonLogro'])) {
+					$logroE = $_POST["logrosE"];
 					$id = $_POST["idE"];
-					$consulta = mysqli_query($BD,"SELECT num FROM Logros WHERE id = '$id'"); 
+					$consulta = mysqli_query($BD,"SELECT * FROM premium WHERE id_usuario = '$id'"); 
 					$usu = mysqli_fetch_array($consulta);
-					$num = $usu["num"];
-					$num++;
-					mysqli_query($BD,"UPDATE Logros SET num = '$num' WHERE id = '$id' ");
+					$EnumLogros = $usu["logros"];
+					if(!preg_match("/{$logroE}/i",$EnumLogros)){
+						$EnumLogros = $EnumLogros . "," . $logroE;
+						$num = $usu["num_logros"];
+						$num++;
+						mysqli_query($BD,"UPDATE premium SET num_logros = '$num', logros = '$EnumLogros' WHERE id_usuario = '$id' ");
+						echo "<span class ='text1'>Completado</span>";
+					} 
+					else echo "<span class='text2'>Ya tiene este logro</span>";
+
 				}
-				echo " <input type='submit' name='buttonLogro' value='darLogro'/>
-				</form>"
-			?>
+				if(isset($_POST['quitarLogro'])) {
+					$logroE = $_POST["logrosE"];
+					$id = $_POST["idE"];
+					$consulta = mysqli_query($BD,"SELECT * FROM premium WHERE id_usuario = '$id'"); 
+					$usu = mysqli_fetch_array($consulta);
+					$EnumLogros = $usu["logros"];
+					if(preg_match("/{$logroE}/i",$EnumLogros)){
+						$EnumLogrosEliminado = str_replace($logroE ,'',$EnumLogros);
+						$num = $usu["num_logros"];
+						$num--;
+						mysqli_query($BD,"UPDATE premium SET num_logros = '$num', logros = '$EnumLogrosEliminado' WHERE id_usuario = '$id' ");
+						echo "<span class ='text1'>Completado</span>";
+					}
+					else echo "<span class='text2'>No posee este logro</span>";
+				}
+				echo "</h3>";
+				?>
+				</div>
+				<div id = 'select'>
+				<input type='submit' class = 'ButtonD' name='quitarLogro' value='Quitar Logro'/>
+				<input type='submit' class = 'ButtonI' name='buttonLogro' value='Añadir Logro'/>
+				</div>
+				</form>
 			</div>
 
+
+			<form method="post">
+			<div id="wrapper">
+				<div id="menu">
+				<h1><span class = 'text'>C H A T &nbsp C O N &nbsp U S U A R I O</span></h1>
+        		<span class="welcome" >Welcome, <?php echo $_SESSION['login'] ?></span>
+				<span class = 'text'> Elige usuario: </span>
+				<select name = 'idE2' id = 'idE2' type = 'text' >
+				<?php 
+				foreach($cadena as $usuario){
+					echo "<option value='$usuario'>$usuario</option>";
+				}
+				?>
+				</select>
+				<input name='actua' type='submit'  id='actua' value='Actualizar Chat' />
+			</div>
+    		<div id="chatbox"></div>
+			<?php
+				if(isset($_POST['idE3'])) {
+					if(isset($_POST['submitmsg'])) {
+						$fecha = date_create()->format('Y-m-d H:i:s');
+						mysqli_query($BD,"INSERT INTO chat (Origen,Receptor,Contenido,Tiempo,Tipo) VALUES ('$_SESSION[login]','$_POST[idE3]','$_POST[usermsg]','$fecha','E-U') ");
+					}
+				}
+				$data = "";
+				if(isset($_POST['idE2'])) {
+					$data =  "[". $data . $_SESSION["login"] . " 🡺 " . $_POST["idE2"] . "]";
+					$consulta = mysqli_query($BD,"SELECT * FROM chat WHERE (Origen = '$_SESSION[login]' AND Receptor = '$_POST[idE2]') OR (Origen = '$_POST[idE2]' AND Receptor = '$_SESSION[login]') ORDER BY Tiempo ASC ");
+					while($chats = mysqli_fetch_array($consulta)){
+						if($chats["Tipo"] == "E-U")$data = $data . "\n". "🡺 (" . $chats["Tiempo"] . ") " . $chats["Origen"] . ": " . $chats["Contenido"];
+						else $data = $data . "\n". "🡸 [" . $chats["Tiempo"] . "] " . $chats["Origen"] . ": " . $chats["Contenido"];
+					}
+				}else{
+					$data = $data . "Debes Actualizar Chat para ver la información";
+				}
+				echo "<textarea rows= '10' name = 'msg' readonly= 'readonly' class = 'chat'>";
+				echo $data;
+				echo "</textarea>";
+			?>
+        	<input name="usermsg" type="text" id="usermsg" size="63" />
+			<span class = 'text'>D: </span>
+				<select name = 'idE3' id = 'idE3' type = 'text' >
+				<?php
+				foreach($cadena as $usuario){
+					echo "<option value='$usuario'>$usuario</option>";
+				}
+				?>
+				</select>
+        	<input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+    		</form>
+			</div>
 	</main>
     <?php 
-		include("../layout/sidebarDer.php");
-		include("../layout/pie.php");
+		include("sidebarDer.php");
+		include("pie.php");
 	?>
-    </div> <!-- Fin del contenedor -->
+    </div> 
 </body>
 </html>
--- P2/EntrenadorPersonalUsu.php
+++ P2/EntrenadorPersonalUsu.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="estilo.css" />
+<link rel="stylesheet" type="text/css" href="css/estiloD.css" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>login</title>
 </head>
@@ -9,16 +9,11 @@
 <body>
 <div id="contenedor">
     <?php 
-		include("includes/vistas/cabecera.php");
-		include("includes/vistas/menu.php");
+		include("cabecera.php");
+		include("sidebarIzq.php");
 	?>
     <main>
-		<header><h1>Tu nutricionista/entrenador personal</h1></header>
-			
-			<div id = "chat">
-				aqui va un chat
-			</div>
-
+		<header><h1>Tu nutricionista/entrenador personal </h1></header>
 			<div id = "logros">
 			<?php 
 				session_start();
@@ -29,20 +24,69 @@
 					}
 					return $mysqli;
 				}
-				$BD = conectar("localhost","root","","aw");
-				$_SESSION['login'] = 'diego';
-				$consulta = mysqli_query($BD,"SELECT * FROM Logros WHERE id = '$_SESSION[login]'"); 
+				$BD = conectar("localhost","root","","lifety");
+				$_SESSION['login'] = 'Usuario1';
+				$consulta = mysqli_query($BD,"SELECT * FROM premium WHERE id_usuario = '$_SESSION[login]'"); 
 				$usu =  mysqli_fetch_array($consulta);
-				echo $usu["num"];
-				
-			?>
+			?>	
+				<h1><span class = 'text'>T U S &nbsp L O G R O S</span></h1> 
+			<div id = 'selectA'>
+				<h3><span class = 'text'>Número de logros : &nbsp<b><?php echo $usu["num_logros"]?></b></span></h3>
 			</div>
+				<div id = 'selectA'>
+				<?php 
+				$EnumLogros = $usu["logros"];
+				$logro = "";
+				for($i = 0; $i < strlen($EnumLogros); $i++){
+					if($EnumLogros[$i] == ","){
+						$logro = $logro . ".png";
+						echo "<img src='logros/$logro' alt='' width='70' height='70'>";
+						$logro = "";
+					}
+					else $logro = $logro . $EnumLogros[$i];
+					if($i+1 == strlen($EnumLogros)){
+						$logro = $logro . ".png";
+						echo "<img src='logros/$logro' alt='' width='70' height='70'>";
+					}
+				}
+				?>
+				</div>
+			
+			</div>
+			<form method="post">
+			<div id="wrapper">
+				<div id="menu">
+				<h1><span class = 'text'>C H A T &nbsp E N T R E N A D O R</span></h1>
+        		<span class="welcome">Bienvenido, <b><?php echo $_SESSION["login"] ?></b>
+				&nbsp &nbsp Tu entrenador es  <b><?php echo $usu["id_profesional"];?></b>
+			</span>
+			</div>
+    		<div id="chatbox"></div>
+			<?php
+				if(isset($_POST['submitmsg'])) {
+					$fecha = date_create()->format('Y-m-d H:i:s');
+					mysqli_query($BD,"INSERT INTO chat (Origen,Receptor,Contenido,Tiempo,Tipo) VALUES ('$_SESSION[login]','$usu[id_profesional]','$_POST[usermsg]','$fecha','U-E') ");
+				}
 
+				$data = "[" . $_SESSION["login"] . " 🡺 " . $usu["id_profesional"] . "]";
+				$consulta = mysqli_query($BD,"SELECT * FROM chat WHERE (Origen = '$_SESSION[login]' AND Receptor = '$usu[id_profesional]') OR (Origen = '$usu[id_profesional]' AND Receptor = '$_SESSION[login]') ORDER BY Tiempo ASC ");
+				while($chats = mysqli_fetch_array($consulta)){
+					if($chats["Tipo"] == "E-U")$data = $data . "\n". "🡸 [" . $chats["Tiempo"] . "] " . $chats["Origen"] . ": " . $chats["Contenido"];
+					else $data = $data . "\n". "🡺 [" . $chats["Tiempo"] . "] " . $chats["Origen"] . ": " . $chats["Contenido"];
+					
+				}
+				echo "<textarea rows= '10' name = 'msg' readonly= 'readonly' class = 'chat'>";
+				echo $data;
+				echo "</textarea>";
+			?>
+        	<input name="usermsg" type="text" id="usermsg" size="63" />
+			<input type="submit"  name="submitmsg" value="send"/>
+			</div>
+		</form>
 	</main>
     <?php 
-		include("includes/vistas/anuncios.php");
-		include("includes/vistas/pie.php");
+		include("sidebarDer.php");
+		include("pie.php");
 	?>
     </div> <!-- Fin del contenedor -->
 </body>
-</html>
