44f57f0a8ca53c28abef72c22f4ebbd26a2fe405
DiegoAA00 <diegoa02@ucm.es>
2022-03-17 17:35:47 +0100

    Update ChatEntrenador.php

--- P2/includes/ChatEntrenador.php
+++ P2/includes/ChatEntrenador.php
@@ -37,7 +37,7 @@ class  ChatEntrenador {
     function mostrarChat(){
         $BD = Aplicacion::getInstance()->getConexionBd();
         $usuactual = $_SESSION["alias"];
-        
+        $id_usuario =  $_SESSION["id"];
        // $usuactual = "Entrenador1";
 
         $dataChat = "";
