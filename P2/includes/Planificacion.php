01c870e5ea548cbd86663362029d669c9961211f
sramos075 <99352465+sramos075@users.noreply.github.com>
2022-03-17 12:11:29 +0100

    cambios para rutina (posible version final)

--- P2/includes/Planificacion.php
+++ P2/includes/Planificacion.php
@@ -2,19 +2,25 @@
 namespace es\ucm\fdi\aw;
 
 class Planificacion {
-
+/*
+Repeticiones por musculo
+    -->Fuerza:6
+    -->Hipertrofia: 10
+    -->Resistencia: 16
+Nº series: 3
+ */
     public function __construct() {}
     public function mostrar(){
         $BD = Aplicacion::getInstance()->getConexionBd();
-        $sqlselect = sprintf("SELECT * FROM planificacion WHERE planificacion.id_usuario = '%s'", $_SESSION['id']);
+        $sqlselect = sprintf("SELECT * FROM planificacion WHERE planificacion.id_usuario = '%d'", $_SESSION['id']);
         $resultado = $BD->query($sqlselect); 
         $fila = $resultado->fetch_assoc();
+        $objetivo = $fila['eobjetivo'];
         $arrayaux = json_decode($fila['rutina']);
         $ejerciciostotales = count($arrayaux [count($arrayaux)-1]); // DIA 1 A 3 MISMOS EJERCICIOS DIA 4 A 5 MAS EJERCICIOS
-        //eof :)))))))))))))
         $contenido = "<caption>Rutina de entrenamiento:</caption><tr>";
-        for ($i = 0; $i < count($arrayaux);$i++){ //nº de dias
-            $contenido .= "<th>Día $i</th>";
+        for ($i = 1; $i < count($arrayaux)+1;$i++){ //nº de dias
+            $contenido .= "<th>Día $i </th>";
         }
         $contenido .= "</tr>";
         for ($i = 0; $i < $ejerciciostotales;$i++){
@@ -25,9 +31,18 @@ class Planificacion {
             }
             $contenido .= "</tr>";
         }
-        //$contenido .= "</table>"; <table>$contenido</table>
+        if($objetivo == 1){
+            $repeticiones = "Nº de repeticiones = 6.  ";
+        }else if($objetivo == 2){
+            $repeticiones = "Nº de repeticiones = 10.  ";
+        }
+        else{
+            $repeticiones = "Nº de repeticiones = 16.  ";
+        }
+        $repeticiones .= "Nº de series: 3";
         $html = <<<EOF
         <table>$contenido</table>
+        $repeticiones
         EOF;
         return $html;
     }
--- P2/includes/Rutina.php
+++ P2/includes/Rutina.php
@@ -14,17 +14,18 @@ class Rutina {
 
     public function comprobarRutina() { 
         $BD = Aplicacion::getInstance()->getConexionBd();
-
         $sqlselect = "SELECT * FROM planificacion WHERE planificacion.id_usuario = '$this->_id'";
         $resultado = $BD->query($sqlselect); 
         if($resultado){
             if($resultado->num_rows > 0){
                 $fila = $resultado->fetch_assoc();
-                if($fila['objetivo'] != $this->_objetivo || $fila['nivel'] != $this->nivel || $fila['dias'] != $this->dias)
-                    this->crearRutina(true);
+                if($fila['eobjetivo'] != $this->_objetivo || $fila['nivel'] != $this->_nivel || 
+                    $fila['dias'] != $this->_dias)
+                    $this->crearRutina(true);
             }
-            else $this->crearRutina(false); 
-            
+            else {
+                $this->crearRutina(false); 
+            }
         }
         else error_log("Error BD ({$this->conn->errno}): {$this->conn->error}");
         
@@ -35,29 +36,29 @@ class Rutina {
         $cont = 1;  
         for ($i = 1; $i < $this->_dias+1; $i++) {
             $arrayaux = array();
-            $stringaux = "";
             if ($i == 4) $cont = 1;
             if ($i >= 1 && $i <= 3) {
-               $this->fill_array($cont, 2, $arrayaux, $stringaux);
+               $this->fill_array($cont, 2, $arrayaux);
             }
             else {
-                $this->fill_array($cont, 3, $arrayaux, $stringaux);
+                $this->fill_array($cont, 3, $arrayaux);
             }
            array_push($this->_array, $arrayaux);
         }
         $guardar = json_encode($this->_array);
         $BD = Aplicacion::getInstance()->getConexionBd();
-        if(!$update) 
-            $query = "INSERT INTO planificacion (id_usuario, rutina, eobjetivo, dias, nivel) VALUES ('$this->_id', '$guardar', '$this->_objetivo', '$this->_dia', '$this->_nivel')";
-        else
-            $query = "UPDATE planificacion SET planificacion.rutina = '$guardar', planificacion.nivel = '$this->_nivel', planificacion.dias = '$this->_dia',  planificacion.eobjetivo = '$this->_objetivo'";
-
+        if(!$update) {
+            $query = "INSERT INTO planificacion (id_usuario, rutina, eobjetivo, dias, nivel) VALUES ('$this->_id', '$guardar', '$this->_objetivo', '$this->_dias', '$this->_nivel')";
+        }
+        else{
+            $query = "UPDATE planificacion SET planificacion.rutina = '$guardar', planificacion.nivel = '$this->_nivel', planificacion.dias = '$this->_dias',  planificacion.eobjetivo = '$this->_objetivo'";
+        }
         if ($BD->query($query)) return true;
         else error_log("Error BD ({$conn->errno}): {$conn->error}");
     }
 
 
-    private function fill_array(&$cont, $nveces , &$arrayaux, &$stringaux) {
+    private function fill_array(&$cont, $nveces , &$arrayaux) {
 
         $BD = Aplicacion::getInstance()->getConexionBd();
         for ($i = 0; $i < $nveces; $i++){
@@ -68,11 +69,6 @@ class Rutina {
             while ($fila = $resultado->fetch_assoc()){
                 if($j < $this->_ejerciciosdia){
                     array_push($arrayaux, $fila['nombre']); 
-                    if($j+1 == $this->_ejerciciosdia && $i+1 == $nveces) $stringaux .= $fila['nombre'];
-                    else {
-                        $stringaux .= $fila['nombre'];
-                        $stringaux .= " | ";
-                    }
                 } 
                 $j++;
             }
@@ -81,7 +77,6 @@ class Rutina {
     }
 
     public function __construct($id, $objetivo, $nivel, $dias) {
-
         $this->_nivel = $nivel;
         $this->_id = $id;
         $this->_objetivo = $objetivo;
