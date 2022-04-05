c9d6242b99cd7b47d183a9f84a9e1a3f9b72f304
Ivanlcrv <ivanlcrv@gmail.com>
2022-04-05 13:21:48 +0200

    css login y register sin terminar cuidado

--- P3/includes/FormularioLogin.php
+++ P3/includes/FormularioLogin.php
@@ -9,11 +9,14 @@ class FormularioLogin extends Formulario {
     protected function generaCamposFormulario(&$datos) {
         // Se reutiliza el nombre de usuario introducido previamente o se deja en blanco
         $alias = $datos['alias'] ?? '';
-
+        $nombre = $datos['nombre'] ?? '';
+        $apellidos = $datos['apellidos'] ?? '';
+        $mail = $datos['mail'] ?? '';
         // Se generan los mensajes de error si existen.
         $htmlErroresGlobales = self::generaListaErroresGlobales($this->errores);
         $erroresCampos = self::generaErroresCampos(['alias', 'password'], $this->errores, 'span', array('class' => 'error'));
-        
+        $erroresCampos = self::generaErroresCampos(['nombre', 'apellidos', 'mail', 'alias', 'password', 'password2'], $this->errores, 'span', array('class' => 'error'));
+
         // Se genera el HTML asociado a los campos del formulario y los mensajes de error.
         $img = RUTA_IMGS;
         $ruta = RUTA_APP;
@@ -21,15 +24,34 @@ class FormularioLogin extends Formulario {
         $htmlErroresGlobales
         <div class=form>
             <div class="thumbnail">
-                <img src='$img/login.jpg' alt=thumbnail/>
+                <img src='$img/login.jpg' alt=thumbnail />
             </div>
-            <p class="error">{$erroresCampos['alias']}</p>
-            <input id="alias" type="text" name="alias" value="$alias" placeholder="usuario" />
-            <p class="error">{$erroresCampos['password']}</p>
-            <input id="password" type="password" name="password" placeholder="password"/>
-            <button type="submit" name="login">Entrar</button>
-            <p class="message">¿No estas registrado? <a href='$ruta/registro.php'>Crea una cuenta</a></p>
+            <div class=login>
+                <p class="error">{$erroresCampos['alias']}</p>
+                <input id="alias" type="text" name="alias" value="$alias" placeholder="usuario" />
+                <p class="error">{$erroresCampos['password']}</p>
+                <input id="password" type="password" name="password" placeholder="password"/>
+                <button type="submit" name="login">Entrar</button>
+                <p class="message">¿No estas registrado? <a href='#'>Crea una cuenta</a></p>
+            </div>
+            <div class=login id=register>
+                <p class="error">{$erroresCampos['nombre']}</p>
+                <input id="nombre" type="text" name="nombre" value="$nombre" placeholder="nombre" />
+                <p class="error">{$erroresCampos['apellidos']}</p>
+                <input id="apellidos" type="text" name="apellidos" value="$apellidos" placeholder="apellidos" />
+                <p class="error">{$erroresCampos['alias']}</p>
+                <input id="alias" type="text" name="alias" value="$alias" placeholder="usuario" />
+                <p class="error">{$erroresCampos['mail']}</p>
+                <input id="mail" type="text" name="mail" value="$mail" placeholder="correo electronico" />
+                <p class="error">{$erroresCampos['password']}
+                <input id="password" type="password" name="password" placeholder="password" />
+                <p class="error">{$erroresCampos['password2']}</p>
+                <input id="password2" type="password" name="password2" placeholder="reintroduce la password" />
+                <button type="submit" name="registro">Registrar</button>
+                <p class="message">¿Ya estas registrado? <a href='$ruta/login.php'>Logeate.</a></p>
+            </div>
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         EOF;
         return $html;
     }
