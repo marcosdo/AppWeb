9b665171a67014596f04d11bb809d691aa9cc1ce
amagarzog <amagarzo@ucm.es>
2022-04-11 13:27:59 +0200

    cambios formulario 1 editar rutina

--- P3/includes/clases/appweb/plan/FormularioPlanEntRutina.php
+++ P3/includes/clases/appweb/plan/FormularioPlanEntRutina.php
@@ -15,17 +15,20 @@ class FormularioPlanEntRutina extends Formulario {
         $query = sprintf("SELECT * FROM entrena WHERE nutri = '%s'",$entNombre); 
         $rs = $conn->query($query); 
         while($fila = $rs->fetch_assoc()){
-            $query2 = sprintf("SELECT * FROM usuario WHERE usuario = '%s'",$fila['usuario']);            
+            $query2 = sprintf("SELECT * FROM usuario WHERE id_usuario = '%s'",$fila['usuario']);            
             $rs2 = $conn->query($query2); 
             $filausuario = $rs2->fetch_assoc();
-            $query3 = sprintf("SELECT * FROM rutina WHERE id_usuario = '%s'",$filausuario['id_usuario']);          
-            $rs3 = $conn->query($query3); 
-            $row_cnt = $rs3->num_rows;
-            if($row_cnt >0) $rts = $rts ."<option value='$fila[usuario]'>$fila[usuario]</option>";
+            if($filausuario != null){
+                $query3 = sprintf("SELECT * FROM rutina WHERE id_usuario = '%s'",$filausuario['id_usuario']);          
+                $rs3 = $conn->query($query3); 
+                $row_cnt = $rs3->num_rows;
+                if($row_cnt >0) $rts = $rts ."<option value='$fila[usuario]'>$fila[usuario]</option>";
+            }
         }
 
-        $rs->free();
-        return $rts;
+       if($rts != "") $rs->free();
+        
+       return $rts;
     }
 
     protected function generaCamposFormulario(&$datos) {
@@ -36,21 +39,24 @@ class FormularioPlanEntRutina extends Formulario {
         $erroresCampos = self::generaErroresCampos(['alias'], $this->errores, 'span', array('class' => 'error'));
     
         $SelectUsuarios = self::Usuarios($_SESSION['alias']);
+        $boton ="";
+        if($SelectUsuarios == "") $Select = "<div><p>No hay usuarios disponibles.</p></div>";
+        else {
+            $Select = 
+            "<p> Selecciona el usuario: </p><div><select name = 'alias' id = 'alias' type = 'text'>";
+            $Select .= $SelectUsuarios;
+            $Select .= "</select></div>";
+            $boton = "<div><button type='submit' name='enviar'>Editar rutina</button></div>";
+        }
         // Se genera el HTML asociado a los campos del formulario y los mensajes de error.
         $html = <<<EOF
         $htmlErroresGlobales
         <fieldset id ="formreditautina"> 
             <legend id="edit-routine-plan">Editor de Rutinas</legend>
-            <p> Selecciona el usuario: </p>
-                    <div>
-                    <select name = 'alias' id = 'alias' type = 'text'>
-                        $SelectUsuarios
-                    </select>
-                    </div>
+                        $Select
                     {$erroresCampos['alias']}
-                    <div>
-                    <button type="submit" name="enviar">Editar rutina</button>
-                    </div>
+                        $boton
+                    
         </fieldset>
         EOF;
         return $html;
