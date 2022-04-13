35a00f5de3a93de46c15bd22811d1757010293fa
amagarzog <amagarzo@ucm.es>
2022-04-13 13:42:10 +0200

    Create planes.php

---
+++ P3/includes/vistas/helpers/planes.php
@@ -0,0 +1,116 @@
+<?php
+use appweb\plan\Rutina;
+use appweb\plan\Dieta;
+
+
+/**
+     * Metodo que devuelve una tabla con el contenido de las rutinas de la base de datos
+     * @return string $html Código en html de una tabla
+*/
+
+function mostrarRutina(){
+    
+    $obj = 10;
+    $arrayreps = [];
+    $arrayaux = Rutina::buscaRutina($obj, $arrayreps, $_SESSION['id']);
+    if($obj != 10){
+        $ejerciciostotales = count($arrayaux [count($arrayaux)-1]); // DIA 1 A 3 MISMOS EJERCICIOS DIA 4 A 5 MAS EJERCICIOS
+        $contenido = "<caption>Rutina de entrenamiento</caption><thead><tr>";
+
+
+        for ($i = 1; $i < count($arrayaux)+1;$i++){ //nº de dias
+            $contenido .= "<th>Día $i </th>";
+        }
+        $contenido .= "</tr></thead><tbody>";
+        for ($i = 0; $i < $ejerciciostotales;$i++){
+            $contenido .= "<tr>";
+            for ($j = 0; $j < count($arrayaux); $j++) { //nº de ejercicios al cabo del día
+                $auxiliar = isset($arrayaux[$j][$i]) ? $arrayaux[$j][$i] : ""; //DIA 4 Y 5 HASTA 6 Y DIA 1 A 3 HASTA 4 EN NIVEL PRINCIPIANTE :)
+                if(isset($arrayaux[$j][$i])) $auxiliar .= " x ";
+                $auxiliar .= isset($arrayreps[$j][$i])  ? $arrayreps[$j][$i] : "";
+                $contenido .= "<td> $auxiliar</td>";
+            }
+            $contenido .= "</tr>";
+        }
+        $series = "</tbody><div id= repeticiones>";
+        $series .= "<p> Nº de series: 3 </p> </div>";
+        $html = <<<EOF
+        <table id=planificacion>$contenido</table>
+        $series
+        EOF;
+    }
+    else{
+        $contenido = "No hay ninguna rutina creada";
+
+        $html = <<<EOF
+
+        $contenido
+        EOF;
+    }
+    return $html;
+
+}
+
+/**
+     * Metodo que devuelve una tabla con el contenido de las comidas de la base de datos
+     * La tabla tiene una columna por dia de la semana, y dos filas por cada comida { desayuno, comida, cena }
+     * @return string $html Código en html de una tabla
+*/
+
+function mostrarDieta(){
+    $dias = 0;
+    $desayuno = array();
+    $almuerzo = array();
+    $cena = array();
+    $fecha = Dieta:: buscaDieta($_SESSION['id'], $dias, $desayuno, $almuerzo, $cena);
+
+    $contenido = "<table id=planificacion>";
+    $contenido .= "<caption>Planificacion de tu dieta:</caption>";
+    $contenido .= "<thead><tr>";
+    // Dias de la semana 
+    $contenido .= "<th></th>";
+    for ($i = 0; $i < $dias; $i++) { 
+        $dia_semana = date('w', strtotime($fecha));
+        $dia_mes = date('d', strtotime($fecha));
+        switch ($dia_semana) {
+            case 1: { $contenido .= "<th>L ".$dia_mes."</th>";   } break;
+            case 2: { $contenido .= "<th>M ".$dia_mes."</th>";   } break;
+            case 3: { $contenido .= "<th>X ".$dia_mes."</th>";   } break;
+            case 4: { $contenido .= "<th>J ".$dia_mes."</th>";   } break;
+            case 5: { $contenido .= "<th>V ".$dia_mes."</th>";   } break;
+            case 6: { $contenido .= "<th>S ".$dia_mes."</th>";   } break;
+            case 0: { $contenido .= "<th>D ".$dia_mes."</th>";   } break;
+            default: break;
+        }
+        $fecha = date('Y-m-d', strtotime($fecha . '+1 day'));
+    }
+    $contenido .= "</tr></thead><tbody>";
+    for ($i = 0; $i < 3; $i++) {
+        $contenido .= "<tr>";
+        switch ($i) {
+            case 0: $contenido .= "<td id=\"table-diets\">Desayuno</td>";  break;
+            case 1: $contenido .= "<td id=\"table-diets\">Comida</td>";    break;
+            case 2: $contenido .= "<td id=\"table-diets\">Cena</td>";      break;
+            default: break;
+        }
+        for ($j = 0; $j < $dias; $j++) {
+            switch ($i) {
+                case 0: $contenido .= "<td>" . $desayuno[$j] . "</td>";   break;
+                case 1: $contenido .= "<td>" . $almuerzo[$j] . "</td>";     break;
+                case 2: $contenido .= "<td>" . $cena[$j] . "</td>";       break;
+                default: break;
+            }
+        }
+        $contenido .= "</tr>";
+    }
+    $contenido .= "</tbody></table>";
+    $html = <<<EOF
+    $contenido
+    EOF;
+
+    return $html;
+
+}
+
+
+
