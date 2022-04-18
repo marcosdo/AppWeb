aeff87611253ce4d9c84964e634cdaf9d08b0339
Ivanlcrv <ivanlcrv@gmail.com>
2022-04-18 23:08:42 +0200

    errores

--- P3/suscripcion.php
+++ P3/suscripcion.php
@@ -9,9 +9,6 @@ $tituloPagina = 'Pago';
 $ruta = RUTA_IMGS;
 $contenidoPrincipal = <<<EOS
 <h1>Contratacion de nutricionista</h1>
-<div id="tabla">
 $htmlFormPago
-<img src="$ruta/nutricionista.jpg" alt="Tu nutri de confianza" >
-</div>
 EOS;
 require __DIR__.'/includes/vistas/plantillas/plantilla.php';
