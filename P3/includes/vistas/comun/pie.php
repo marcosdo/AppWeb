2daa74c4ee6a26283e7a1a88837da5cac31bd89e
Leyretxu <leyrehf@hotmail.com>
2022-04-18 19:56:31 +0200

    Update pie
    Pie más simple pero con gridlayout

--- P3/includes/vistas/comun/pie.php
+++ P3/includes/vistas/comun/pie.php
@@ -1,75 +1,10 @@
+<footer>
 
-<table id="tablefooter">
-<tr>
-  <td id="col1">
-      <div id="columna1">
-                    
-
-                   <h1>About us</h1>
-
-                    <div class="fila_footer">
-                         <label> Más información sobre nosotros</label>
-                    </div>
-
-                    <div class="fila_footer2">
-                        <label><b>(c) 2022 Todos los derechos reservados | LIFETY</b></label>
-                    </div>
-
-      </div>
-    </td>
     
-  <td>
-      <div class="columna2">
+            <h1>About us</h1>
+            <p>+34 656 18 03 47</p>
+            <p>lifety@gmail.com</p>
+            <p><b>(c) 2022 Todos los derechos reservados | LIFETY</b></p>
 
-                <h1>Redes sociales</h1>
 
-                <div class="fila_footer">
-                    <img src="src/img/logos/Facebook.jpg" alt="Facebook" > 
-                    <label>Síguenos en Facebook</label>
-                </div>
-
-                <div class="fila_footer">
-                    <img src="src/img/logos/Instagram.jpg" alt="Instagram" >
-                    <label>Síguenos en Instagram</label>
-                </div>
-
-                <div class="fila_footer">
-                    <img src="src/img/logos/twitter.jpg" alt="twitter" >
-                    <label>Síguenos en Twitter</label>
-                </div>
-
-                <div class="fila_footer2">
-                    <label><b>Informacion Lifety</b></label>
-                
-                </div>
-
-        </div>
-    </td>
-  <td>
-      <div class="columna3">
-                    <h1>Contacto</h1>
-
-                    <div class="fila_footer">
-                        <img src"">
-                        <label>Calle Salud 14,
-                                28100
-                                Alcobendas, Madrid</label>
-                    </div>
-
-                    <div class="fila_footer">
-                        <img src"">
-                        <label>+34 656 18 03 47</label>
-                    </div>
-
-                    <div class="fila_footer">
-                        <img src="src/img/logos/Gmail.jpg" alt="Gmail" >    
-                        <label>lifety@gmail.com</label>
-                    </div>
-
-                    <div class="fila_footer2">
-                        <label><b>Politica de Privacidad | Terminos y condiciones</b></label>
-                    </div>
-
-                </div>
-    </td>
-</tr>
+</footer>
