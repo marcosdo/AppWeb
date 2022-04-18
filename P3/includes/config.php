40936cc63d77a3a3a99f9dc3d1cff0e41eaa8238
sramos075 <99352465+sramos075@users.noreply.github.com>
2022-04-18 16:26:25 +0200

    Poner el config como antes

--- P3/includes/config.php
+++ P3/includes/config.php
@@ -12,7 +12,7 @@ define('BD_PASS', 'lifetypass');
  * Parámetros de configuración utilizados para generar las URLs y las rutas a ficheros en la aplicación
  */
 define('RAIZ_APP', __DIR__);
-define('RUTA_APP', '/AppWeb/P3');
+define('RUTA_APP', '/AW/GitHub/P3');
 define('RUTA_IMGS', RUTA_APP.'/src/img');
 define('RUTA_CSS', RUTA_APP.'/src/css');
 define('RUTA_JS', RUTA_APP.'/src/js');
