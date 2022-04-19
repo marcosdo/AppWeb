96d353d67ac3bd3fd484329de49eccc5595b0e69
DiegoAA00 <diegoa02@ucm.es>
2022-04-19 16:57:45 +0200

    Eliminación de logros
    no funciona en la vpn, lo arreglare en un futuro por ahora la funcionalidad se queda con el chat exclusivamente.

--- P3/chatprof.php
+++ P3/chatprof.php
@@ -5,14 +5,10 @@ require_once __DIR__.'/includes/config.php';
 $chatUsuario = new appweb\chat\MostrarChatEntrenador();
 $MostrarChat = $chatUsuario->mostrarChat();
 
-$FormularioLogros = new appweb\chat\MostrarLogrosEntrenador();
-$MostrarLogros = $FormularioLogros->mostrarLogrosEnt();
-
 $tituloPagina = 'Entrenador Personal';
 $contenidoPrincipal = <<<EOS
 <h1>Entrenador personal</h1>
 <form method='post' id = "formChatEntrenador">
-	$MostrarLogros
 	$MostrarChat
 </form>
 EOS;
--- P3/chatusu.php
+++ P3/chatusu.php
@@ -5,14 +5,10 @@ require_once __DIR__.'/includes/config.php';
 $chatUsuario = new appweb\chat\MostrarChatUsuario();
 $MostrarChat = $chatUsuario->mostrarChat();
 
-$Logros = new appweb\chat\MostrarLogrosUsuario();
-$MostarLogros = $Logros->mostrarLogrosUsu();
-
 $tituloPagina = 'Tu nutricionista';
 $contenidoPrincipal = <<<EOS
 <h1>Tu nutricionista</h1>
 	<form method="post" id = "formChatUsuario">
-		$MostarLogros
 		$MostrarChat
 	</form>
 EOS;
