86eacb8849ddcb99873b3fe5349f27528a66edbf
Marcos Docampo <marcosdo@ucm.es>
2022-04-19 12:09:59 +0200

    Documentada utils.php

--- P3/includes/clases/appweb/utils.php
+++ P3/includes/clases/appweb/utils.php
@@ -1,26 +1,45 @@
 <?php
+// ==================== CONSTANTES ====================
+// ====================            ====================
+const NUTRI_ROLE = 1;
+const USER_ROLE = 2;
 
-function generaErroresGlobalesFormulario($errores)
-{
-	$html = '';
-	$keys = array_filter(array_keys($errores), function($v) {
-		return is_numeric($v);
-	});
-	if (count($keys) > 0) {
-		$html = '<ul class="errores">';
-		foreach($keys as $key) {
-			$html .= "<li>{$errores[$key]}</li>";
-		}
-		$html .= '</ul>';
-	}
-	return $html;
+// ==================== MÉTODOS ====================
+// ====================         ====================
+
+/**
+ * Genera el html necesario para meter los errores globales en un elemento
+ * @param array $errores array de errores generados
+ */
+function generaErroresGlobalesFormulario($errores) {
+    $html = '';
+    $keys = array_filter(array_keys($errores), function($v) {
+        return is_numeric($v);
+    });
+    if (count($keys) > 0) {
+        $html = '<ul class="errores">';
+        foreach($keys as $key) {
+            $html .= "<li>{$errores[$key]}</li>";
+        }
+        $html .= '</ul>';
+    }
+    return $html;
 }
 
-function generarError($campo, $errores)
-{
+/**
+ * Genera el html necesario para meter los errores particulares en un elemento
+ * @param string     $campo nombre del error
+ * @param array      $errores array de errores generados
+ */
+function generarError($campo, $errores) {
     return isset($errores[$campo]) ? "<span class=\"form-field-error\">{$errores[$campo]}</span>": '';
 }
 
+/**
+ * Crea un componente html por cada campo que haya
+ * @param string     $campo nombre del error
+ * @param array      $errores array de errores generados
+ */
 function generaErroresCampos($campos, $errores) {
     $erroresCampos = [];
     foreach($campos as $campo) {
@@ -28,6 +47,3 @@ function generaErroresCampos($campos, $errores) {
     }
     return $erroresCampos;
 }
-
-const NUTRI_ROLE = 1;
-const USER_ROLE = 2;
