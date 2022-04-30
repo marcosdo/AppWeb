8912532f619c88ae7c362a8f5d78f0909fc9236c
amagarzog <amagarzo@ucm.es>
2022-04-30 13:10:21 +0200

    verproductos

--- P4/includes/clases/appweb/productos/Productos.php
+++ P4/includes/clases/appweb/productos/Productos.php
@@ -24,10 +24,29 @@ class Productos {
      * guardar en una lista todos los productos para usuarios sin plan
      * @return array
      */
-    public static function getProductos(){
+    public static function getProductos(&$ids, &$nombres, &$empresa, &$descripcion, &$precio, &$link, &$tipo){
+        $ids = array();
+        $nombres = array();
+        $empresa = array();
+        $descripcion = array();
+        $precio = array();
+        $link = array();
+        $tipo = array(); 
         $conn = Aplicacion::getInstance()->getConexionBd();
         $query = sprintf("SELECT * FROM productos");
         $rs = $conn->query($query);
+        while($fila = $rs->fetch_assoc()){ 
+            array_push($ids, $fila["id_producto"]);
+            array_push($nombres, $fila["nombre"]);
+            array_push($descripcion, $fila["descripcion"]);
+            array_push($precio, $fila["precio"]);
+            array_push($link, $fila["link"]);
+            array_push($tipo, $fila["tipo"]);
+            $queryempresa = sprintf("SELECT * FROM empresas WHERE empresas.id_empresa = %d", $fila["id_empresa"]);
+            $rsempresa = $conn->query($queryempresa);
+            $filaempresa = $rsempresa->fetch_assoc();
+            array_push($empresa, $filaempresa["nombre"]);
+        } 
         
         return $rs; 
     }
--- P4/includes/vistas/helpers/productos.php
+++ P4/includes/vistas/helpers/productos.php
@@ -6,10 +6,35 @@ use appweb\productos\Productos;
  * @return html
  */
 function listaProductos() {
-    Productos::getProductos();
-    $html = "<br>Lista productos...";
-    
+    $ids = array();
+    $nombres = array();
+    $empresa = array();
+    $descripcion = array();
+    $precio = array();
+    $link = array();
+    $tipo = array(); 
+    Productos::getProductos($ids, $nombres, $empresa, $descripcion, $precio, $link, $tipo);
+    $html = "<div class ='productos'>";
+    $ruta = RUTA_IMGS;
 
+    for($i = 0; $i < count($nombres); $i++){
+        $productoi = "<div class ='row-productos'>";
+        $productoi .= "<h1 class='nombre-producto'>$nombres[$i]</h1>";
+        $productoi .= "<h3>De $empresa[$i]</h3>";
+
+        $productoi .= "<img src='$ruta/productos/$ids[$i].png' alt=$nombres[$i]>";
+        
+        $productoi .= "<h3>$precio[$i]</h3>";
+        $productoi .= "<h3>$tipo[$i]</h3>";
+
+        $productoi .= "<h3>$descripcion[$i]</h3>";
+        $productoi .= "<h3>Para comprar: $link[$i]</h3>";
+
+        $productoi .= "</div>";
+        $html .= $productoi;
+    }
+    
+    $html .= "</div>";
     return $html;
 }
 
