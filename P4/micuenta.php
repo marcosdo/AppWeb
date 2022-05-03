2000680b354be74a09186eb9959fc75e928f4fdd
sramos075 <99352465+sramos075@users.noreply.github.com>
2022-05-03 21:38:13 +0200

    Update micuenta.php sigue fallando

--- P4/micuenta.php
+++ P4/micuenta.php
@@ -2,11 +2,20 @@
 use appweb\Aplicacion;
 use appweb\usuarios\Usuario;
 
+require_once __DIR__.'/includes/config.php';
+
 $app = Aplicacion::getInstance();
 $myID = $app->idUsuario();
 $user = Usuario::buscaPorId($myID);
+if($user){
+    $formContenido = new appweb\usuarios\FormularioEditaUsuario($user);
+    $htmlFormContenido = $formContenido->gestiona();
+}
 
 $tituloPagina = 'Mi Cuenta';
 $contenidoPrincipal = <<<EOS
     <h3>Datos de la cuenta</h3>
+    $htmlFormContenido
 EOS;
+
+require __DIR__.'/includes/vistas/plantillas/plantilla.php';
