29bac4f3a205b1eaad4583ed266c38e6277a8f63
amagarzog <amagarzo@ucm.es>
2022-05-11 22:57:00 +0200

    vistasplani compruebauser

--- P4/plan.php
+++ P4/plan.php
@@ -2,21 +2,29 @@
 
 require_once __DIR__.'/includes/config.php';
 
-$tituloPagina = 'Planificacion';
+if ($app->usuarioLogueado() == true){
 
-$form = new appweb\plan\FormularioRutinas();
-$form2 = new appweb\plan\FormularioDietas();
+    $tituloPagina = 'Planificacion';
 
-$htmlFormRutinas = $form->gestiona();
-$htmlFormDietas = $form2->gestiona();
+    $form = new appweb\plan\FormularioRutinas();
+    $form2 = new appweb\plan\FormularioDietas();
 
-$tituloPagina = 'Planificacion';
- 
-$contenidoPrincipal = <<<EOS
-<div class=plan>
-$htmlFormRutinas
-$htmlFormDietas
-</div>
-EOS;
+    $htmlFormRutinas = $form->gestiona();
+    $htmlFormDietas = $form2->gestiona();
+
+    $tituloPagina = 'Planificacion';
+    
+    $contenidoPrincipal = <<<EOS
+    <div class=plan>
+    $htmlFormRutinas
+    $htmlFormDietas
+    </div>
+    EOS;
+}
+else {
+        header('Location: login.php');
+        exit();
+}
+      
 
 require __DIR__.'/includes/vistas/plantillas/plantilla.php';
--- P4/verplan.php
+++ P4/verplan.php
@@ -1,22 +1,27 @@
 <?php
 
 require_once __DIR__.'/includes/config.php';
+if ($app->usuarioLogueado() == true){
+    $tituloPagina = 'Planificacion';
 
-$tituloPagina = 'Planificacion';
+    $formRutina = new appweb\plan\FormularioVerRutina();
+    $formDieta = new appweb\plan\FormularioVerDieta();
 
-$formRutina = new appweb\plan\FormularioVerRutina();
-$formDieta = new appweb\plan\FormularioVerDieta();
+    $htmlFormRutina = $formRutina->gestiona();
+    $htmlFormDieta = $formDieta->gestiona();
 
-$htmlFormRutina = $formRutina->gestiona();
-$htmlFormDieta = $formDieta->gestiona();
-
-$tituloPagina = 'Ver planificaciones';
- 
-$contenidoPrincipal = <<<EOS
-<div class=verplan>
-$htmlFormDieta
-$htmlFormRutina
-</div>
-EOS;
+    $tituloPagina = 'Ver planificaciones';
+    
+    $contenidoPrincipal = <<<EOS
+    <div class=verplan>
+    $htmlFormDieta
+    $htmlFormRutina
+    </div>
+    EOS;
+}
+else {
+  header('Location: login.php');
+  exit();
+}
 
 require __DIR__.'/includes/vistas/plantillas/plantilla.php';
